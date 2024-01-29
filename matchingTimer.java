89cdbc206083dfe42d50525c5f3973dab78df135
rush <seth187@gmail.com>
2024-01-28 20:32:53 -0500

    Adding my first project to Git!

--- matchingTimer.java
+++ matchingTimer.java
@@ -1,27 +1,39 @@
+import java.util.Scanner;
+
 public class matchingTimer {
 
     public static void main(String[] args) {
         System.out.println("This program will match the last digit of a timer when it is ascending and descending at the same time.");
+        System.out.print("Please specify the number of seconds you want to check: ");
+        Scanner myObj = new Scanner(System.in);
+        String getTheInfo = myObj.nextLine();
+        System.out.println("You entered " + getTheInfo + ".");
 
-        int descendingTimer = 1018;
+        int descendingTimer = Integer.parseInt(getTheInfo);
         int ascendingTimer = 0;
+        int totalSum = 0;
+        int timersAreEqual = 0;
 
         while (descendingTimer >= 0) {
-            //System.out.println("The descending timer is set to: " + descendingTimer);
-            //System.out.println("The ascending timer is set to: " + ascendingTimer);
             descendingTimer--;
             ascendingTimer++;
 
             if (descendingTimer == ascendingTimer) {
                 System.out.println("The timers are equal.");
                 System.out.println("The descending timer equals " + descendingTimer + " and the ascending timer equals " + ascendingTimer + ".");
+                totalSum = totalSum + 1;
             }//if
 
             if (descendingTimer % 10 == ascendingTimer % 10) {
                 System.out.println("Descending " + descendingTimer);
                 System.out.println("Ascending " + ascendingTimer);
+                timersAreEqual = timersAreEqual + 1;
+
             } // modulo op if
 
         } //while
+        System.out.println("The timers are equal this many times: " + totalSum + ".");
+        System.out.println("The last digit in the timers are equal this many times: " + timersAreEqual + ".");
+
     } //main
 }//class
